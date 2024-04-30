6641399ccca3e0632e1e908f93d8d28230c65527
Muhibbulloh <166902979+muhibprogg@users.noreply.github.com>
2024-04-30 12:34:52 +0500

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  for (const num of args) {
+    while (array.includes(num)) {
+      const idx = array.indexOf(num);
+      if (idx !== -1) array.splice(idx, 1);
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
