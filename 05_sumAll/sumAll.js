5b946fb20caa4a842d669454cff7cfd288cf366f
Muhibbulloh <166902979+muhibprogg@users.noreply.github.com>
2024-04-30 13:39:08 +0500

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (start, finish) {
+  if (start < 0 || finish < 0) return "ERROR";
+  if (typeof start !== "number" || typeof finish !== "number") return "ERROR";
+  if (start > finish) {
+    let temp = start;
+    start = finish;
+    finish = temp;
+  }
+  let sum = 0;
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
